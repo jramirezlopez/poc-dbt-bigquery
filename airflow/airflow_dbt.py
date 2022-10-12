b261c6e12e120e806e59e0f725c584c6cfc22b33
Juan Ramirez <juan.a.ramirez.lopez@gmail.com>
2022-10-12 18:00:40 -0300

    Ejecutar Cloud Run desde Composer con variables en la conexion http

--- airflow/airflow_dbt.py
+++ airflow/airflow_dbt.py
@@ -18,29 +18,30 @@ default_args = {
 }
 
 with airflow.DAG(
-        'composer_http_request_cloud',
+        'composer_http_request_cloud_run',
         'catchup=False',
         default_args=default_args,
         schedule_interval=datetime.timedelta(days=1)) as dag:
 
     print_token = bash.BashOperator(
-        task_id='print_token',
+        task_id='get_token',
         #bash_command='gcloud auth print-identity-token "--audiences=https://us-central1-run.googleapis.com/apis/run.googleapis.com/v1/namespaces/poc-bco-chile/jobs/poc-dbt-image"' # The end point of the deployed Cloud Run container
         bash_command='gcloud auth print-access-token'
     )
 
-    token = "{{ task_instance.xcom_pull(task_ids='print_token') }}" # gets output from 'print_token' task
+    token = "{{ task_instance.xcom_pull(task_ids='get_token') }}" # gets output from 'print_token' task
 
     task_get_op = SimpleHttpOperator(
-        task_id='get_op',
+        task_id='exec_cloud_run_dbt',
         method='POST',
         http_conn_id='cloud_run',
+        endpoint='us-central1-run.googleapis.com/apis/run.googleapis.com/v1/namespaces/poc-bco-chile/jobs/poc-dbt-image:run',
         headers={'Authorization': 'Bearer ' + token },
     )
 
     def process_data_from_http(**kwargs):
         ti = kwargs['ti']
-        http_data = ti.xcom_pull(task_ids='get_op')
+        http_data = ti.xcom_pull(task_ids='exec_cloud_run_dbt')
         print(http_data)
 
     process_data = python.PythonOperator(
